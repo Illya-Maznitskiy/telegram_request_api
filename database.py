025d2879ea23e8ae5b7c53f7f23be3a2175335bc
Illya Maznitskiy <comercaleuros@gmail.com>
2024-12-20 17:18:02 +0200

    Add database.py

---
+++ database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Database URL, change it according to your settings
+DATABASE_URL = (
+    "postgresql://admin_user:admin_password@localhost:5432/fastapi_project_db"
+)
+
+# SQLAlchemy setup
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)  # Adjust for PostgreSQL or other DB
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
+
+
+# Dependency to get the DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
